e7ba60f94fec897cc1e0b3a1bcf9cf00e959bd4c
Tamako Aiba <t.aiba@4nonome.com>
2021-05-15 12:14:39 +0900

    電卓アプリの完成。

--- Caluculator/ViewController.swift
+++ Caluculator/ViewController.swift
@@ -17,6 +17,20 @@ class ViewController: UIViewController {
     
     var ope: Int = 0
     
+    @IBAction func select0(){
+        number1 = number1*10 + 0
+        label.text = String(number1)
+    }
+    @IBAction func select1(){
+        number1 = number1*10 + 1
+        label.text = String(number1)
+    }
+    
+    @IBAction func select2(){
+        number1 = number1*10 + 2
+        label.text = String(number1)
+    }
+    
     @IBAction func select3(){
         number1 = number1*10 + 3
         label.text = String(number1)
@@ -25,12 +39,57 @@ class ViewController: UIViewController {
         number1 = number1*10 + 4
         label.text = String(number1)
     }
+    
+    @IBAction func select5(){
+        number1 = number1*10 + 5
+        label.text = String(number1)
+    }
+    @IBAction func select6(){
+        number1 = number1*10 + 6
+        label.text = String(number1)
+    }
+    @IBAction func select7(){
+        number1 = number1*10 + 7
+        label.text = String(number1)
+    }
+    @IBAction func select8(){
+        number1 = number1*10 + 8
+        label.text = String(number1)
+    }
+    @IBAction func select9(){
+        number1 = number1*10 + 9
+        label.text = String(number1)
+    }
+    
     @IBAction func plus(){
         number2 = number1
         number1 = 0
         ope = 1
-        
     }
+    
+    @IBAction func minus(){
+        number2 = number1
+        number1 = 0
+        ope = 2
+    }
+    
+    @IBAction func times(){
+        number2 = number1
+        number1 = 0
+        ope = 3
+    }
+    
+    @IBAction func divided(){
+        number2 = number1
+        number1 = 0
+        ope = 4
+    }
+    
+    @IBAction func clear(){
+        number1 = 0
+        label.text = String(number1)
+    }
+    
     @IBAction func equal(){
         if ope == 1 {
             number3 = number2 + number1
